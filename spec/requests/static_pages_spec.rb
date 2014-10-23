cce75d18a647a3e3445a2c038a2f07dba6e74ba9
Bratislav Miletic <bimgate@gmail.com>
2014-10-23 05:47:24 -0700

    Add Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-
 describe "StaticPages" do
+
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 include Capybara::DSL
   describe "GET /static_pages" do
@@ -20,7 +21,7 @@ include Capybara::DSL
 
     it "should have the title 'Home'" do
       visit 'static_pages/home'
-       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home") 
+       page.should have_selector('title', :text => "#{base_title} | Home") 
 
 end
 end
@@ -35,7 +36,7 @@ describe "Help page" do
 
     it "should have the title 'Help'" do
     visit 'static_pages/help'
-    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    page.should have_selector('title', :text => "#{base_title} | Help")
   end
 end
 
@@ -47,11 +48,21 @@ describe "About page" do
 
   it "should have the title 'About Us'" do
     visit 'static_pages/about'
-    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    page.should have_selector('title', :text => "#{base_title} | About Us")
   end
 end
 
+describe "Contact page" do
+  it "should have the h1 'Contact'" do
+    visit 'static_pages/contact'
+    page.should have_selector('h1', :text => 'Contact')
+  end
 
+  it "should have the title 'Contact'" do
+    visit 'static_pages/contact'
+    page.should have_selector('title', :text => "#{base_title} | Contact")
+  end
+end
 
 end
 
