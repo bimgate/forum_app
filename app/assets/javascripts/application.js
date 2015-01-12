b8767ff13097f5cb3327f7d9dd56724879fee39c
Bratislav Miletic <bimgate@gmail.com>
2015-01-12 15:35:34 +0100

    Update app drop down menu4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_self
 //= require bootstrap
 //= require jquery
 //= require jquery_ujs
