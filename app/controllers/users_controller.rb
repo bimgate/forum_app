5f0ee323ef00b8d240f9c869b631a53a869edd18
Bratislav Miletic <bimgate@gmail.com>
2015-03-25 12:54:40 +0100

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
   
@@ -59,6 +59,19 @@ flash[:sucess] = "User destroyed."
 redirect_to users_path
 end
 
+def following    
+@title = "Following"
+@user = User.find(params[:id])
+@users = @user.followed_users.paginate(page: params[:page])
+render 'show_follow'
+end
+
+def followers
+@title = "Followers"
+@user = User.find(params[:id])
+@users = @user.followers.paginate(page: params[:page])
+render 'show_follow'
+end
 
   private
 
@@ -82,6 +95,5 @@ end
 def admin_user
 redirect_to(root_path) unless current_user.admin?
 end
-
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,8 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-
+    it { should respond_to(:feed) }
+    it { should respond_to(:relationships) }
 	it { should respond_to(:password_confirmation) }
     it { should respond_to(:remember_token) }
     it { should respond_to(:authenticate) }
